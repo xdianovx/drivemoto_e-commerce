07683f9a69dde82b0893067da7bbbe5fdc1150c2
dianov evgeniy <dianov.js@gmail.com>
2020-08-18 23:09:23 +0300

    Banner section десктоп

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider_btn banner-section__slider_btn-left"><img src="img/icons/arr-left.svg" alt=""></button>\n',
+        nextArrow: '<button class="banner-section__slider_btn banner-section__slider_btn-right"><img src="img/icons/arr-right.svg" alt=""></button>\n',
+    });
+});
