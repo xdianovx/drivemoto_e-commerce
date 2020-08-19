571404cf5c94761ca2b7b059db944431ba78bc08
dianov evgeniy <dianov.js@gmail.com>
2020-08-19 13:44:28 +0300

    Search section десктоп

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,14 @@ $(function () {
         prevArrow: '<button class="banner-section__slider_btn banner-section__slider_btn-left"><img src="img/icons/arr-left.svg" alt=""></button>\n',
         nextArrow: '<button class="banner-section__slider_btn banner-section__slider_btn-right"><img src="img/icons/arr-right.svg" alt=""></button>\n',
     });
+
+    $('.search-tabs__item').on('click', function (e) {
+        e.preventDefault();
+        $('.search-tabs__item').removeClass('active')
+        $('.search-content__item').removeClass('active')
+
+        $(this).addClass('active')
+        $($(this).attr('href')).addClass('active')
+        console.log($($(this).attr('href')));
+    });
 });
